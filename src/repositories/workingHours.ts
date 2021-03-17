fbe91819c90fc7ea023ba5ab2107b5d7b222f595
Yuri Toledo <yuriwtoledo@gmail.com>
2021-03-16 21:17:37 -0300

    chore: splitted repositories and services

---
+++ src/repositories/workingHours.ts
@@ -0,0 +1,36 @@
+import { workingHoursAPI } from '../services/workingHours'
+import { WorkSituation } from '../utils/constants'
+
+type GetWorkerCurrentSituation = Promise<{ situation: WorkSituation } | null>
+
+export const getWorkerCurrentSituation = async (): GetWorkerCurrentSituation => {
+  try {
+    const response = await workingHoursAPI.get('/')
+
+    return response.data
+  } catch (error) {
+    return null
+  }
+}
+
+type PostWorkerSituationParams = {
+  situation: WorkSituation,
+  time: string
+}
+type PostWorkerSituation = Promise<{ nextSituation: WorkSituation } | null>
+
+export const postWorkerSituation = async (
+  params: PostWorkerSituationParams,
+): PostWorkerSituation => {
+  const nextStatus = params.situation === WorkSituation.EXITING
+    ? WorkSituation.ARRIVING
+    : WorkSituation.EXITING
+
+  try {
+    await workingHoursAPI.post('/', params)
+
+    return { nextSituation: nextStatus }
+  } catch (error) {
+    return null
+  }
+}
--- src/services/workingHours.ts
+++ src/services/workingHours.ts
@@ -1,33 +1,5 @@
-import { WorkSituation } from '../utils/constants'
+import axios from 'axios'
 
-const baseURL = 'http://localhost:5000/working-hours'
-
-type GetWorkerCurrentSituationService = Promise<{ situation: WorkSituation } | null>
-
-export const getWorkerCurrentSituationService = async (): GetWorkerCurrentSituationService => {
-  try {
-    const response = await fetch(baseURL)
-    const data = await response.json()
-
-    return data
-  } catch (error) {
-    return null
-  }
-}
-
-export const postWorkerSituationService = async (currSituation: number) => {
-  const nextStatus = currSituation === WorkSituation.EXITING
-    ? WorkSituation.ARRIVING
-    : WorkSituation.EXITING
-
-  const body = JSON.stringify({
-    status: nextStatus,
-  })
-
-  const response = await fetch(baseURL, {
-    method: 'PUT',
-    body,
-  })
-
-  const data = await response.json()
-}
+export const workingHoursAPI = axios.create({
+  baseURL: 'http://localhost:5000/working-hours/',
+})
