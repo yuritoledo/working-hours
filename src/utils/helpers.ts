24daf8d7dfbb7601c214947890934de8d381c2df
Yuri Toledo <yuriwtoledo@gmail.com>
2021-03-17 11:10:22 -0300

    refac: hour helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,3 @@
 import dayjs from 'dayjs'
 
-export const getNowTime = () => dayjs().format('HH:mm:ss')
-
-export const getNowTimex = () => dayjs().format('HH:mm:ss')
+export const getCurrentHour = () => dayjs().format('HH:mm:ss')
