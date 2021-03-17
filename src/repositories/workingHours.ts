08c9f503456c8276aad71a444a6501b1b26f9cd1
Yuri Toledo <yuriwtoledo@gmail.com>
2021-03-17 11:11:37 -0300

    feat: implemented getAllWorkerHour

--- src/repositories/workingHours.ts
+++ src/repositories/workingHours.ts
@@ -1,5 +1,24 @@
+import dayjs, { Dayjs } from 'dayjs'
 import { workingHourService } from '../services/workingHours'
+import { WorkingHours } from '../types/general'
 import { WorkSituation } from '../utils/constants'
+
+const backendProcessing = (registers: WorkingHours[]) => {
+  const format = 'DD MMMM'
+  const uniqueDays = [...new Set(
+    registers.map((reg) => dayjs(reg.date).format(format)),
+  )]
+
+  const filterByDay = (day: string) => registers.filter(
+    (register) => dayjs(register.date).format(format) === day,
+  )
+
+  const days = uniqueDays.map((day) => ({
+    day,
+    list: filterByDay(day),
+  }))
+  return days
+}
 
 type GetWorkerCurrentSituation = Promise<{ situation: WorkSituation } | null>
 
@@ -15,7 +34,7 @@ export const getWorkerCurrentSituation = async (): GetWorkerCurrentSituation => 
 
 type PostWorkerSituationParams = {
   situation: WorkSituation,
-  time: string
+  date: Dayjs
 }
 type PostWorkerSituation = Promise<{ nextSituation: WorkSituation } | null>
 
@@ -30,6 +49,19 @@ export const postWorkerSituation = async (
     await workingHourService.post('/', params)
 
     return { nextSituation: nextStatus }
+  } catch (error) {
+    return null
+  }
+}
+
+export const getAllWorkerHour = async () => {
+  try {
+    const response = await workingHourService.get<WorkingHours[]>('/')
+    const registers = response.data
+
+    const days = backendProcessing(registers)
+
+    return days
   } catch (error) {
     return null
   }
