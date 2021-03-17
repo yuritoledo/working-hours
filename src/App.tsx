cd53403c7baaf241a527f1b757b7f4f1dc5d8db5
Yuri Toledo <yuriwtoledo@gmail.com>
2021-03-16 21:18:05 -0300

    feat: create GlobalStyle and added to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Toaster } from 'react-hot-toast'
 import ActionPanel from './components/ActionPanel/ActionPanel'
 import History from './components/History/History'
+import { GlobalStyles } from './styles/GlobalStyles'
 
 const App = () => (
   <>
+    <GlobalStyles />
     <ActionPanel />
     <History />
+    <Toaster
+      position="top-center"
+    />
   </>
 )
 
