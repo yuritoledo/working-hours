7fcc0d6dd163b82c14dcb541b201706faeb56bae
Yuri Toledo <yuriwtoledo@gmail.com>
2021-03-18 12:01:45 -0300

    feat: add more helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,17 @@
 import dayjs from 'dayjs'
 
 export const getCurrentHour = () => dayjs().format('HH:mm:ss')
+
+export const isOdd = (value: number) => value % 2 !== 0
+
+export const timeConvert = (num: number) => {
+  const rawHour = Math.floor(num / 60)
+  const rawMinutes = num % 60
+
+  const valueWithZeroAtLeft = (value: number) => String(value).padStart(2, '0')
+
+  const hour = valueWithZeroAtLeft(rawHour)
+  const minutes = valueWithZeroAtLeft(rawMinutes)
+
+  return `${hour}:${minutes}`
+}
