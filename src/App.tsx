7d96c87a61ba569fc6722ed9613a17606ce51875
Yuri Toledo <yuriwtoledo@gmail.com>
2021-03-18 12:01:15 -0300

    feat: add Container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { Toaster } from 'react-hot-toast'
+import styled from 'styled-components'
 import ActionPanel from './components/ActionPanel/ActionPanel'
 import History from './components/History/History'
-import { GlobalStyles } from './styles/GlobalStyles'
+
+const Container = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  background-color: #eee;
+  display: flex;
+`
 
 const App = () => (
-  <>
-    <GlobalStyles />
+  <Container>
     <ActionPanel />
     <History />
-    <Toaster
-      position="top-center"
-    />
-  </>
+    <Toaster position="top-center" />
+  </Container>
 )
 
 export default App
