072d6de28faa7f6c8db93cc2f88c4417cb7964f4
Yuri Toledo <yuriwtoledo@gmail.com>
2021-03-19 17:36:41 -0300

    refactor: moved logic to "backend"

--- src/repositories/workingHours.ts
+++ src/repositories/workingHours.ts
@@ -1,7 +1,33 @@
 import dayjs, { Dayjs } from 'dayjs'
+import _chunk from 'lodash.chunk'
 import { workingHourService } from '../services/workingHours'
-import { WorkingHours } from '../types/general'
+import { HistoryList, WorkingHours } from '../types/general'
 import { WorkSituation } from '../utils/constants'
+import { timeConvert } from '../utils/helpers'
+
+type Register = WorkingHours[]
+
+const backendProcessingWorkingTime = (register: HistoryList) => {
+  const workingTimeInMinutes = register.reduce((totalHoursInDay, currentDay) => {
+    const period = _chunk(currentDay.list, 2)
+
+    const workedHoursInDay = period.reduce((totalHoursInPeriod, currentPeriod) => {
+      const isIncompletePeriod = currentPeriod.length === 1
+      if (isIncompletePeriod) return totalHoursInPeriod
+
+      const [arriving, exiting] = currentPeriod
+
+      const workedTime = dayjs(exiting.date).diff(arriving.date, 'minutes')
+
+      return totalHoursInPeriod + workedTime
+    }, 0)
+
+    return totalHoursInDay + workedHoursInDay
+  }, 0)
+
+  const workedTime = timeConvert(workingTimeInMinutes)
+  return workedTime
+}
 
 const getNextSituation = (situation: WorkSituation) => {
   if (situation === WorkSituation.EXITING) {
@@ -11,7 +37,7 @@ const getNextSituation = (situation: WorkSituation) => {
   return WorkSituation.EXITING
 }
 
-const backendProcessing = (registers: WorkingHours[]) => {
+const backendProcessingToList = (registers: Register) => {
   const format = 'DD MMMM'
   const uniqueDays = [...new Set(
     registers.map((reg) => dayjs(reg.date).format(format)),
@@ -38,6 +64,7 @@ export const getWorkerCurrentSituation = async (): GetWorkerCurrentSituation => 
         _order: 'desc',
       },
     })
+    console.log(response)
     const lastSituation = response.data[0].situation
     const nextSituation = getNextSituation(lastSituation)
     return nextSituation
@@ -68,16 +95,12 @@ export const postWorkerSituation = async (
 
 export const getAllWorkerHour = async () => {
   try {
-    const response = await workingHourService.get<WorkingHours[]>('/', {
-      params: {
-        _sort: 'id',
-        _order: 'desc',
-      },
-    })
+    const response = await workingHourService.get<WorkingHours[]>('/')
     const registers = response.data
-    const days = backendProcessing(registers)
+    const registerList = backendProcessingToList(registers)
+    const workedTime = backendProcessingWorkingTime(registerList)
 
-    return days
+    return { registerList, workedTime }
   } catch (error) {
     return null
   }
