dd0d1ff95614c6f40870a985024a324f56ea4c52
Yuri Toledo <yuriwtoledo@gmail.com>
2021-03-19 17:32:49 -0300

    styles: improvments

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyles = createGlobalStyle`
  body {
   font-family: Roboto, sans-serif;
+  color: #333;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
