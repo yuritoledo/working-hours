1d1b11a9ed17dc71b649bc7ad580d07a0162c8ad
Yuri Toledo <yuriwtoledo@gmail.com>
2021-03-19 14:17:38 -0300

    refactor: move component to own file

---
+++ src/components/Section.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Section = styled.section`
+  min-width: 30vw;
+  min-height: 80vh;
+
+  padding: 1rem 3rem;
+  margin-top: 1rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  border-radius: 32px;
+  background-color: white;
+  box-shadow: 0 0 7px #999;
+`
