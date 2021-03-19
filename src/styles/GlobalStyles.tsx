1b87d93b5f32c333397fcefe84e7fbb45b0b7a1c
Yuri Toledo <yuriwtoledo@gmail.com>
2021-03-19 14:16:29 -0300

    refactor: fixing global style

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,5 +1,10 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
+ body {
   font-family: Roboto, sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+ }
 `
