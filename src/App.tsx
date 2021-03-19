3f884e1658066857b7b12235d0560a3a8a1b422b
Yuri Toledo <yuriwtoledo@gmail.com>
2021-03-19 17:32:17 -0300

    style: improving responsivity

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { Toaster } from 'react-hot-toast'
 import styled from 'styled-components'
 import ActionPanel from './components/ActionPanel/ActionPanel'
 import History from './components/History/History'
+import { GlobalStyles } from './styles/GlobalStyles'
 
 const Container = styled.div`
+  background-color: #efefff;
   min-height: 100vh;
-  min-width: 100vw;
-  background-color: #eee;
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  @media(min-width: 800px) {
+    flex-direction: row;
+  }
 `
 
 const App = () => (
@@ -15,6 +21,7 @@ const App = () => (
     <ActionPanel />
     <History />
     <Toaster position="top-center" />
+    <GlobalStyles />
   </Container>
 )
 
