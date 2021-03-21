ef536f7ac0854e22a07d3ce5cd21c467d7c2261f
Yuri Toledo <yuriwtoledo@gmail.com>
2021-03-20 21:49:56 -0300

    refactor: apply AppContainer import

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
 import { Toaster } from 'react-hot-toast'
-import styled from 'styled-components'
 import ActionPanel from './components/ActionPanel/ActionPanel'
+import { AppContainer } from './components/AppContainer/AppContainer'
 import History from './components/History/History'
 import { GlobalStyles } from './styles/GlobalStyles'
 
-const Container = styled.div`
-  background-color: #efefff;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
-  @media(min-width: 1200px) {
-    flex-direction: row;
-  }
-`
-
 const App = () => (
-  <Container>
+  <AppContainer>
     <ActionPanel />
     <History />
     <Toaster position="top-center" />
     <GlobalStyles />
-  </Container>
+  </AppContainer>
 )
 
 export default App
